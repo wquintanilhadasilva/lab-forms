101734a06c7a371ae845fb3fad344b2590a449b1
Wedson Quintanilha <wquintanilhadasilva@gmail.com>
2017-05-29 18:16:27 -0300

    Destacando campos inválidos com css & estados angular

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'wedson',
-    email: 'wedson@email.com',
-    endereco: ''
+    nome: null,
+    email: null,
+    endereco: null
   };
 
   constructor() { }
