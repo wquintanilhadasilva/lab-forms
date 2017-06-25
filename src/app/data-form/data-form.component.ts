0be31780a096236170b36f641c5bdf7adea469fa
Wedson Quintanilha <wquintanilhadasilva@gmail.com>
2017-06-25 17:37:24 -0300

    aula 97

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -23,18 +23,23 @@ export class DataFormComponent implements OnInit {
 
     /*this.formulario = new FormGroup({
       nome: new FormControl(null),
-      email: new FormControl(null)
+      email: new FormControl(null),
+      endereco: new FormGroup({
+        cep: new FormControl(null)
+      })
     });*/
     this.formulario = this.formBuilder.group({
       nome: [null, Validators.required],
       email: [null, [Validators.required, Validators.email]],
-      cep: [null, Validators.required],
-      numero: [null, Validators.required],
-      complemento: [null],
-      rua: [null, Validators.required],
-      bairro: [null, Validators.required],
-      cidade: [null, Validators.required],
-      estado: [null, Validators.required]
+      endereco: this.formBuilder.group({
+        cep: [null, Validators.required],
+        numero: [null, Validators.required],
+        complemento: [null],
+        rua: [null, Validators.required],
+        bairro: [null, Validators.required],
+        cidade: [null, Validators.required],
+        estado: [null, Validators.required]
+      })
     });
   }
 
